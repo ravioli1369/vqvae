5064cc975fff89a73b0035b22e3d0f50f9b4ffa1
Ravi Kumar <ravioli1369@gmail.com>
2024-10-29 05:43:25 +0000

    put stuff in if __name__ == "__main__"

--- vqvae/codebooks.py
+++ vqvae/codebooks.py
@@ -9,7 +9,6 @@ import torch.optim as optim
 import utils
 from models.vqvae import VQVAE
 
-parser = argparse.ArgumentParser()
 
 """
 Hyperparameters
@@ -23,32 +22,30 @@ def extract_codebook(model_path, model):
     return model.vector_quantization.embedding.weight.detach().cpu().numpy()
 
 
-parser.add_argument("--n_hiddens", type=int, default=128)
-parser.add_argument("--n_residual_hiddens", type=int, default=32)
-parser.add_argument("--n_residual_layers", type=int, default=2)
-parser.add_argument("--embedding_dim", type=int, default=8)
-parser.add_argument("--n_embeddings", type=int, default=64)
-parser.add_argument("--beta", type=float, default=0.25)
-
-parser.add_argument("--filename", type=str, default=timestamp)
-parser.add_argument("--model_path", type=str, default=None)
-
-args = parser.parse_args()
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-model = VQVAE(
-    args.n_hiddens,
-    args.n_residual_hiddens,
-    args.n_residual_layers,
-    args.n_embeddings,
-    args.embedding_dim,
-    args.beta,
-).to(device)
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_hiddens", type=int, default=128)
+    parser.add_argument("--n_residual_hiddens", type=int, default=32)
+    parser.add_argument("--n_residual_layers", type=int, default=2)
+    parser.add_argument("--embedding_dim", type=int, default=8)
+    parser.add_argument("--n_embeddings", type=int, default=64)
+    parser.add_argument("--beta", type=float, default=0.25)
+
+    parser.add_argument("--filename", type=str, default=timestamp)
+    parser.add_argument("--model_path", type=str, default=None)
+
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = VQVAE(
+        args.n_hiddens,
+        args.n_residual_hiddens,
+        args.n_residual_layers,
+        args.n_embeddings,
+        args.embedding_dim,
+        args.beta,
+    ).to(device)
     codebook = extract_codebook(args.model_path, model)
     print(codebook.shape)
     np.save(args.filename, codebook)
