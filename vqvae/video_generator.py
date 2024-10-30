b3d0a0e626695a7ba68f24c105800dbe937147b2
Ravi Kumar <ravioli1369@gmail.com>
2024-10-30 21:33:31 +0000

    refactor for new directory structure

--- vqvae/video_generator.py
+++ vqvae/video_generator.py
@@ -1,19 +1,22 @@
 import imageio
 import os
 
-dist = ["speckle"]
-metrics = ["pca", "lle", "mds", "isomap", "tsne"]
+dist = ["gaussian", "poisson", "speckle", "original"]
+metrics = ["pca", "tsne", "mds", "isomap", "lle", "kde"]
+for d1 in dist:
+    for d2 in dist:
+        for m in metrics:
+            if d1 == d2:
+                continue
+            image_folder = f"/home/exouser/Documents/vqvae/video/{d1}_{d2}/{m}/"
+            output_video_path = (
+                f"/home/exouser/Documents/vqvae/video/{d1}_{d2}/{d1}_{d2}_{m}.mp4"
+            )
 
-for d in dist:
-    for m in metrics:
-        print(f"Generating video for {m} with {d} distribution")
-        image_folder = f"../results/video/og_{d}/{m}"
-        output_video_path = f"../results/video/og_{d}/output_video_{m}_{d}.mp4"
+            images = []
+            for i in range(0, 2000, 10):
+                filename = os.path.join(image_folder, f"{i}.png")
+                if os.path.exists(filename):
+                    images.append(imageio.imread(filename))
 
-        images = []
-        for i in range(0, 2000, 10):
-            filename = os.path.join(image_folder, f"{i}.png")
-            if os.path.exists(filename):
-                images.append(imageio.imread(filename))
-
-        imageio.mimsave(output_video_path, images, fps=15)
+            imageio.mimsave(output_video_path, images, fps=15)
